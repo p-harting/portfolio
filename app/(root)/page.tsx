d9a0d539e23b1110784532ab471ff2a993af953f
p-harting <pascalharting98@gmail.com>
2025-05-09 11:58:59 +0200

    Add basic css styling to Hero component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex justify-center items-center mt-8">
       <Hero />
     </main>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,18 +1,30 @@
+import Image from "next/image"
+
 export default function Hero() {
     return (
-        <div>
-            <div>
-                <h2>Fullstack Engineer aus Deutschland</h2>
-                <h1>Hallo, mein Name ist Pascal Harting</h1>
-                <div>
+        <div className="flex flex-row max-w-6xl mt-12">
+            <div className="flex flex-col max-w-[60%] gap-4">
+                <h2 className="text-white text-3xl"><span className="text-purple-500">Fullstack Engineer</span> aus Deutschland</h2>
+                <h1 className="text-white text-7xl">Hallo, mein Name ist <span className="text-purple-500">Pascal Harting</span></h1>
+                <div className="border border-solid border-white rounded-4xl py-4 px-8 text-white text-xl w-2xs">
                     <p>Lebenslauf downloaden</p>
                 </div>
-                <p>Ich entwickel schnelle und skalierbare Apps mit dem Optimierungsfokus eines Marketers.</p>
+                <p className="text-white text-xl">Ich entwickel <span className="underline">schnelle</span> und <span className="underline">skalierbare</span> Apps mit dem Optimierungsfokus eines Marketers.</p>
             </div>
             <div>
+                <div className="flex items-center justify-end gap-2 border border-solid border-white rounded-4xl py-4 px-8">
+                    <div className="relative">
+                        <div className="h-3 w-3 bg-green-500 rounded-full animate-ping opacity-75 absolute"></div>
+                        <div className="h-3 w-3 bg-green-500 rounded-full relative"></div>
+                    </div>
+                    <p className="text-white text-xl">Open to work</p>
+                </div>
                 <div>
-                    <div></div>
-                    <p>Open to work</p>
+                    <Image
+                    src="/hero.png"
+                    alt="An image of me"
+                    width={389}
+                    height={389} />
                 </div>
             </div>
         </div>
