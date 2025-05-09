3e5701c8ba330c61b55be9a7d7b887e5b3f77798
p-harting <pascalharting98@gmail.com>
2025-05-09 16:37:36 +0200

    Implement Supabase and render example data on portfolio page

--- app/(root)/portfolio/page.tsx
+++ app/(root)/portfolio/page.tsx
@@ -1,6 +1,23 @@
-export default function Page() {
+import { createClient } from '@/utils/supabase/server';
+
+export default async function Page() {
+    const supabase = await createClient();
+    const { data } = await supabase.from("projects").select();
+    console.log(data)
+
+    const projects = data?.map(project => {
+      return(
+        <div key={project.id}>
+          <h2 className="text-white">{project.name}</h2>
+        </div>
+      )
+    })
+
     return (
-      <h1 className="text-8xl text-white">PORTFOLIO</h1>
+      <>
+        <h1 className="text-8xl text-white">PORTFOLIO</h1>
+        {projects}
+      </>
     );
   }
   
