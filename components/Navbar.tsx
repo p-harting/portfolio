195d9ea6c607be3f6d88ed3f007d370528f992e4
p-harting <pascalharting98@gmail.com>
2025-05-09 10:15:29 +0200

    Add basic css styles to Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,18 @@
 export default function Navbar() {
     return (
-        <nav>
-            <div>
-                <ul>
-                    <li>Home</li>
-                    <li>Portfolio</li>
-                    <li>Über mich</li>
-                    <li>Blog</li>
-                    <li>Kontakt</li>
-                </ul>
+        <nav className="flex justify-between items-center mt-8">
+            <div className="flex-1 flex justify-center">
+                <div className="border border-solid border-white rounded-4xl py-4 px-8">
+                    <ul className="flex text-white text-xl gap-6">
+                        <li>Home</li>
+                        <li>Portfolio</li>
+                        <li>Über mich</li>
+                        <li>Blog</li>
+                        <li>Kontakt</li>
+                    </ul>
+                </div>
             </div>
-            <div>
+            <div className="flex text-white border border-solid border-white rounded-4xl py-4 px-8">
                 <p>DE</p>
             </div>
         </nav>
