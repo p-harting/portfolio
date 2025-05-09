6d84f9df048e3894a90bfe101e191f8b7ee1e985
p-harting <pascalharting98@gmail.com>
2025-05-09 12:13:18 +0200

    Create routes for portfolio, about, blog and contact

---
+++ app/(root)/blog/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <h1 className="text-8xl text-white">BLOG</h1>
+  );
+}
---
+++ app/(root)/kontakt/page.tsx
@@ -0,0 +1,6 @@
+export default function Page() {
+    return (
+      <h1 className="text-8xl text-white">KONTAKT</h1>
+    );
+  }
+  
