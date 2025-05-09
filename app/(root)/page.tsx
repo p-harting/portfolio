b8ab4c505ad95c18f004d52c4d061a974dc7ff88
p-harting <pascalharting98@gmail.com>
2025-05-09 10:58:18 +0200

    Create basic hero component and add to home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Portfolio</h1>
-    </div>
+    <main>
+      <Hero />
+    </main>
   );
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,20 @@
+export default function Hero() {
+    return (
+        <div>
+            <div>
+                <h2>Fullstack Engineer aus Deutschland</h2>
+                <h1>Hallo, mein Name ist Pascal Harting</h1>
+                <div>
+                    <p>Lebenslauf downloaden</p>
+                </div>
+                <p>Ich entwickel schnelle und skalierbare Apps mit dem Optimierungsfokus eines Marketers.</p>
+            </div>
+            <div>
+                <div>
+                    <div></div>
+                    <p>Open to work</p>
+                </div>
+            </div>
+        </div>
+    )
+}
