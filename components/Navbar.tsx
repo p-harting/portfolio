6f56fdd685cb4e218569d34d9dd6de23ebc08806
p-harting <pascalharting98@gmail.com>
2025-05-09 12:16:51 +0200

    Link routes in Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,26 @@
+import Link from "next/link"
+
 export default function Navbar() {
     return (
         <nav className="flex justify-between items-center mt-8">
             <div className="flex-1 flex justify-center">
                 <div className="border border-solid border-white rounded-4xl py-4 px-8">
                     <ul className="flex text-white text-xl gap-6">
-                        <li>Home</li>
-                        <li>Portfolio</li>
-                        <li>Über mich</li>
-                        <li>Blog</li>
-                        <li>Kontakt</li>
+                        <li>
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link href="/portfolio">Portfolio</Link>
+                        </li>
+                        <li>
+                            <Link href="/ueber-mich">Über mich</Link>
+                        </li>
+                        <li>
+                            <Link href="/blog">Blog</Link>
+                        </li>
+                        <li>
+                            <Link href="/kontakt">Kontakt</Link>
+                        </li>
                     </ul>
                 </div>
             </div>
