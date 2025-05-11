7555809643b9ec46d6c2b48cfe9898bffadc8239
p-harting <pascalharting98@gmail.com>
2025-05-11 12:23:12 +0200

    Create and implement translations for Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,50 +1,48 @@
 import { Dictionary, Locale } from "@/i18n-config";
 import Link from "next/link";
 
-export default function Footer({ dictionary, currentLang }: { 
-  dictionary: Dictionary["footer"], 
-  currentLang: Locale 
-}) {
+export default function Footer({ dictionary }: { dictionary: Dictionary["footer"] })
+{
     return (
       <div className="flex flex-col bg-linear-to-t from-black to-[#3C0366] border-t-1 border-white">
         <div className="flex">
           <div className="text-white">
             <p>Pascal Harting</p>
-            <p>Full Stack Software Engineer</p>
-            <p>Building scalable platforms and AI-driven tools, balancing technical rigor with creative problem-solving. Passionate about user-centric solutions.</p>
+            <p>{dictionary.job}</p>
+            <p>{dictionary.description}</p>
           </div>
           <div className="text-white">
             <p>Navigation</p>
             <ul>
               <li>
                 <Link href="/">
-                  Home
+                  {dictionary.home}
                 </Link>
               </li>
               <li>
                 <Link href="/portfolio">
-                  Portfolio
+                  {dictionary.portfolio}
                 </Link>
               </li>
               <li>
                 <Link href="/ueber-mich">
-                  About
+                  {dictionary.about}
                 </Link>
               </li>
               <li>
                 <Link href="/blog">
-                  Blog
+                  {dictionary.blog}
                 </Link>
               </li>
               <li>
                 <Link href="/kontakt">
-                  Contact
+                  {dictionary.contact}
                 </Link>
               </li>
             </ul>
           </div>
           <div className="text-white">
-            <p>Socials</p>
+            <p>{dictionary.socials}</p>
             <ul>
               <li>
                 <Link href="https://www.linkedin.com/in/pascal-harting">
@@ -61,7 +59,7 @@ export default function Footer({ dictionary, currentLang }: {
         </div>
         <div className="text-white">
             <hr />
-            <p>Made with ♡ by Pascal Harting</p>
+            <p>{dictionary.made}</p>
         </div>
       </div>
     )
